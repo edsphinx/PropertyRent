bfd12a251228bc331b32fbf0a993c4f14f970cbe
Oscar Fonseca <edsphinx@gmail.com>
2023-05-19 23:27:38 -0600

    feat: added Modal UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/navbar/navbar';
+import ClientOnly from './components/client-only';
+import Modal from './components/modals/modal';
 
 export const metadata = {
 	title: 'Airbnb',
@@ -19,7 +21,14 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={font.className}>
-				<Navbar />
+				<ClientOnly>
+					<Modal
+						actionLabel='Submit'
+						title='Mensaje Prueba'
+						isOpen
+					/>
+					<Navbar />
+				</ClientOnly>
 				{children}
 			</body>
 		</html>
