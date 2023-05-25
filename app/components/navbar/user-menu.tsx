19b78436011ac9541af3d60577b8e322c09cd886
Oscar Fonseca <edsphinx@gmail.com>
2023-05-25 05:20:57 -0600

    feat: add Favorites Page

--- app/components/navbar/user-menu.tsx
+++ app/components/navbar/user-menu.tsx
@@ -62,7 +62,7 @@ const UserMenu = ({ currentUser }: UserMenuProps) => {
 									label='My Trips'
 								/>
 								<MenuItem
-									onClick={() => {}}
+									onClick={() => router.push('/favorites')}
 									label='My Favorites'
 								/>
 								<MenuItem
---
+++ app/favorites/page.tsx
@@ -0,0 +1,44 @@
+import EmptyState from '../components/empty-state';
+import ClientOnly from '../components/client-only';
+
+import getCurrentUser from '../actions/get-current-user';
+import getFavoriteListings from '../actions/get-favorite-listings';
+import FavoritesClient from './favorites-client';
+
+const ListingPage = async () => {
+	const listings = await getFavoriteListings();
+	const currentUser = await getCurrentUser();
+
+	if (listings.length === 0) {
+		return (
+			<ClientOnly>
+				<EmptyState
+					title='No favorites found'
+					subtitle='Looks like you have no favorite listings.'
+				/>
+			</ClientOnly>
+		);
+	}
+
+	if (!currentUser) {
+		return (
+			<ClientOnly>
+				<EmptyState
+					title='Unauthorized'
+					subtitle='Please login'
+				/>
+			</ClientOnly>
+		);
+	}
+
+	return (
+		<ClientOnly>
+			<FavoritesClient
+				listings={listings}
+				currentUser={currentUser}
+			/>
+		</ClientOnly>
+	);
+};
+
+export default ListingPage;
